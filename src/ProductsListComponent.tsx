e5315eca8421dc02eddf72877b16d0245de9197c
Willis Morse <willismorse@mac.com>
2018-02-12 10:23:12 -0500

    initial commit

---
+++ src/ProductsListComponent.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export interface ProductsListComponentProps {
+}
+
+export interface ProductsListComponentState {
+}
+
+export default class ProductsListComponent extends React.Component<ProductsListComponentProps, ProductsListComponentState> {
+  constructor(props: ProductsListComponentProps) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        asd
+      </div>
+    );
+  }
+}
+
