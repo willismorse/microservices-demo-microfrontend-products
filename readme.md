b4087ed25b8da1baa2debe0482c0e8459033f4c5
Willis Morse <willismorse@threnodyne.com>
2018-03-10 12:26:40 -0500

    Added 404, liveness and readiness support

--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 This code provides a microfrontend for the Sock Shop microservices demo project. 
+
+
+# Commands
+
+docker build -t 'willismorse/microservices-demo-microfrontend-products:latest' .
+docker push willismorse/microservices-demo-microfrontend-products:latest
+
+helm package microservices-demo-microfrontend-products-chart
+helm repo index . 
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('build'))
+// Map incoming path requests '/products' to our '/public' folder. In other words, live in the subdirectory 'products'
+app.use('/products',express.static('build'))
+
+// Add explicit routing support for liveness and readiness probes. Note that these probes don't care about response content, they just want to see a response code of 200
+app.use('/products/liveness', express.static('build/index.html'))
+app.use('/products/readiness', express.static('build/index.html'))
+
+app.use(function(req, res, next) {
+    res.status(404).send('products microfrontend: could not find resource: ' + req.url);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
