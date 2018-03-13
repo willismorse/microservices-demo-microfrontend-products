eeefbf9f044c280cc6fc2293d33e0d93f9a77461
Willis Morse <willismorse@threnodyne.com>
2018-03-13 16:00:19 -0400

    Converted from CreateReactApp infrastructure to bespoke webpack 4 build system

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-// Map incoming path requests '/products' to our '/public' folder. In other words, live in the subdirectory 'products'
-app.use('/products',express.static('build'))
+/*
+// For testing purposes, live in the root directory
+app.use(express.static('./public'))
+app.use(express.static('./dist'))
+*/
+
+// Map incoming path requests '/products' to our '/public' and '/dist' folders. In other words, live in the subdirectory 'products'
+app.use('/products',express.static('public'));
+app.use('/products',express.static('dist'));
 
 // Add explicit routing support for liveness and readiness probes. Note that these probes don't care about response content, they just want to see a response code of 200
-app.use('/products/liveness', express.static('build/index.html'))
-app.use('/products/readiness', express.static('build/index.html'))
+app.get('/products/liveness', (req, res) => res.send('Live!'));
+app.use('/products/readiness', (req, res) => res.send('Ready!'));
+
 
 app.use(function(req, res, next) {
     res.status(404).send('products microfrontend: could not find resource: ' + req.url);
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(8080, () => console.log('microfrontend-hostapp listening on port 8080!'))
+app.listen(8080, () => console.log('microfrontend-hostapp listening on port 8081!'));
